26d98cd836aa5a37fc3790d2f5a4c489d9b2a60c
Supreeth Murthy <supreeths84@gmail.com>
2016-07-05 00:14:26 -0700

    First commit

---
+++ setup.sh
@@ -0,0 +1,4 @@
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_91.jdk/Contents/Home
+export PATH=/Users/supreethmurthy/Documents/Software/apache-maven-3.3.9/bin:$PATH
+echo $PATH
+echo $JAVA_HOME
